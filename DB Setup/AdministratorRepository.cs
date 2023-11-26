551bc117e24d8f37708c7a74d264b364566de975
tarashoshko <tarikhoshko@gmail.com>
2023-11-26 18:20:50 +0200

    Fix: made methods virtual

--- DB Setup/AdministratorRepository.cs
+++ DB Setup/AdministratorRepository.cs
@@ -5,24 +5,24 @@ public class AdministratorRepository
 {
     private readonly InventoryContext _context = new InventoryContext();
 
-    public Administrator GetAdministratorByEmail(string email)
+    public virtual Administrator GetAdministratorByEmail(string email)
     {
 
         return _context.administrators.FirstOrDefault(a => a.email_address == email);
     }
 
-    public List<Administrator> GetAdministratorsByCompanyName(string company_name)
+    public virtual List<Administrator> GetAdministratorsByCompanyName(string company_name)
     {
         return _context.administrators.Where(a => a.company_name == company_name).ToList();
     }
 
-    public List<Administrator> GetFilteredAdmins(string searchTerm, string company_name)
+    public virtual List<Administrator> GetFilteredAdmins(string searchTerm, string company_name)
     {
         return _context.administrators.Where(a => a.company_name == company_name && a.full_name.Contains(searchTerm))
             .ToList();
     }
 
-    public bool Create(Administrator administrator)
+    public virtual bool Create(Administrator administrator)
     {
         bool userExists = _context.administrators.Any(a => a.email_address == administrator.email_address);
 
@@ -36,7 +36,7 @@ public class AdministratorRepository
         return false;
     }
 
-    public void Update(Administrator administrator)
+    public virtual void Update(Administrator administrator)
     {
         _context.Entry(administrator).State = EntityState.Modified;
         _context.SaveChanges();
